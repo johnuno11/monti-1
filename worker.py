e88353ba80dc6851c0371509d134b5b9a4232bfe
John Malouf <github@maloufs.org>
2016-12-01 10:11:26 -0700

    fixed worker

--- worker.py
+++ worker.py
@@ -12,9 +12,14 @@ print('[*] Waiting for messages. To exit press CTRL+C')
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue='task_queue')
+
 channel.start_consuming()
