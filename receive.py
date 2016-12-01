ca88ec35838be312a99d03a71d3f7582b0e29d20
John Malouf <github@maloufs.org>
2016-12-01 09:36:30 -0700

    Adding receive prog

---
+++ receive.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# receive.py - receive messages from rabbitmq via pika
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('172.19.0.2'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
