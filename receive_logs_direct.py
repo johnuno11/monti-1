62956c82c1a5db3e10a4a812b14a8615fe404b1c
John Malouf <github@maloufs.org>
2016-12-06 09:32:00 -0700

    J

--- receive_logs_direct.py
+++ receive_logs_direct.py
@@ -25,7 +25,7 @@ for severity in severities:
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
-    print(" [x] %r:%r" % (method.routing_key, body)
+    print(" [x] %r:%r" % (method.routing_key, body))
 
 channel.basic_consume(callback,
                       queue=queue_name,
