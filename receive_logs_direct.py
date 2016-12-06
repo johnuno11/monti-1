d164d140d077e8e8862b37f6c3a5ec559f48f6a4
John Malouf <github@maloufs.org>
2016-12-06 10:15:31 -0700

    more syntax err

--- receive_logs_direct.py
+++ receive_logs_direct.py
@@ -18,7 +18,7 @@ if not severities:
     sys.exit(1)
 
 for severity in severities:
-    channel.queue_bind(exchange='direc_logs',
+    channel.queue_bind(exchange='direct_logs',
                        queue=queue_name,
                        routing_key=severity)
 
